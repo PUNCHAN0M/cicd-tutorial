7f46bc002abd846d06c0ba526c74bebbc8c7cd74
PUNCHAN0M <pavananonphanich@gmail.com>
2026-04-29 22:26:16 +0700

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-       <h1 className="text-3xl font-bold">My App Test CI | CD Fix Error</h1>
+       <h1 className="text-3xl font-bold">My App Test CI | CD Pass CI | CD</h1>
       </main>
     </div>
   );
